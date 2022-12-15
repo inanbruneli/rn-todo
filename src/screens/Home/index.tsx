6e832a0822fc36be41204fd05a46874d062379fa
Inan Brunelli <inanbruneli@gmail.com>
2022-12-15 13:23:41 -0300

    feat: style de status e input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { TextInput, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 import Header from '../../components/Header';
 import { styles } from './styles';
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.containerInput}>
-        <TextInput style={styles.input} />
-        <TouchableOpacity style={styles.botao} />
+        <TextInput style={styles.input} placeholder='Digite uma nova tarefa' placeholderTextColor='#808080' />
+        <TouchableOpacity style={styles.botao}>
+          <AntDesign name='pluscircleo' color='#F2F2F2' size={17} />
+        </TouchableOpacity >
+      </View>
+      <View style={styles.containerStatus}>
+        <View style={styles.itemStatus}>
+          <Text style={[styles.textStatus, styles.criadas]}>Criadas</Text>
+          <Text style={styles.numeroStatus}>0</Text>
+        </View>
+        <View style={styles.itemStatus}>
+          <Text style={[styles.textStatus, styles.concluidas]}>Concluidas</Text>
+          <View style={styles.containerNumero}>
+            <Text style={styles.numeroStatus}>0</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
