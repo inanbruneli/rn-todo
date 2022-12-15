5c90e874fd8e5cb7ea6d122e8f799fd796426efd
Inan Brunelli <inanbruneli@gmail.com>
2022-12-15 13:24:05 -0300

    feat: input  e status

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -9,7 +9,8 @@ export const styles = StyleSheet.create({
     padding: 24,
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection:'row'
+    flexDirection:'row',
+    marginTop:-50,
   },
   input :{
     flex:1,
@@ -17,13 +18,47 @@ export const styles = StyleSheet.create({
     backgroundColor: '#262626',
     borderColor:'#0D0D0D',    
     borderWidth:1,
-    borderRadius: 6
+    borderRadius: 6,
+    padding: 16,
+    shadowColor: "rgba(0,0,0,.25)",
+    shadowOffset: { width: 0, height: 2, },
+    shadowOpacity: 0.55,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  botao:{
+  botao:{    
     marginLeft: 4,
     width: 52,
     height: 52,
     backgroundColor: '#1E6F9F',
-    borderRadius: 6
+    borderRadius: 6,
+    justifyContent:'center',
+    alignItems: 'center',
+    flexDirection :'row',
+  },
+  containerStatus:{
+    justifyContent: 'space-between',
+    flexDirection:'row',
+    paddingHorizontal: 24,    
+  },
+  itemStatus:{
+    flexDirection:'row',    
+  },
+  textStatus:{
+    fontWeight:'bold',
+    marginRight:10
+  },
+  criadas:{
+    color:'#4EA8DE'
+  },
+  concluidas:{
+    color:'#8284FA'
+  },
+  numeroStatus:{
+    fontWeight:'bold',    
+  },
+  containerNumero: {
+    backgroundColor:'#333333',
+    
   }
 });
